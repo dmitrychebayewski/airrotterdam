17dd126ab405b3fc90f62a234c1711ac293467b1
dchebayewski <vrij-nederland@yandex.com>
2020-10-25 18:54:36 +0100

    AQ-038 Enabling CircleCI AppEngine deploy (solving compile failure)

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-//import App from '../components/app/App';
+import App from '../components/app/App';
 
 test('Geolocation panel should be present', () => {
-    // const {getByText: applicationRoot} = render(<App/>);
-    // expect(applicationRoot(/Rotterdam near Ahoy/)).toBeInTheDocument();
+    const {getByText: applicationRoot} = render(<App/>);
+    expect(applicationRoot(/Rotterdam near Ahoy/)).toBeInTheDocument();
 });
