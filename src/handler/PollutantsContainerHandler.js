30cfe3268c2599fa4b28042b9725f1603859005a
Dmitry Chebayewski <dmitry.chebayewski@billinghouse.nl>
2020-11-01 14:24:57 +0100

    AQ-000 Small client improvements

--- src/handler/PollutantsContainerHandler.js
+++ src/handler/PollutantsContainerHandler.js
@@ -4,15 +4,26 @@ import {TOP_POLLUTANTS_LIST} from "../metadata/TopPollutants";
 export function updateComponentsMeasurements(reactComponent) {
     GetAggregatedComponentsMeasurements.getComponentsMeasurements(TOP_POLLUTANTS_LIST).then(response => {
             reactComponent.setState(() => {
-                return {
+                return response && response.length? {
                     componentsMeasurements: response,
                     coordinates: {
                         lat: response[0].coordinates[1],
                         lng: response[0].coordinates[0],
                         where: response[0].whereMeasured
                     }
-                }
+                } :newFunction(reactComponent)
             });
         }
     );
+
+    function newFunction(reactComponent) {
+        return {
+            componentsMeasurements: reactComponent.state.componentsMeasurements,
+            coordinates: {
+                lat: reactComponent.state.coordinates.lat,
+                lng: reactComponent.state.coordinates.lng,
+                where: reactComponent.state.coordinates.where
+            }
+        };
+    }
 }
